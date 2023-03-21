c3bfd6417a52e3f696dc638fed90e640807c9980
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-03-21 21:12:22 +0100

    Création du fichier des constantes au projet

---
+++ app/src/main/java/com/example/competitionapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.competitionapp.utils
+
+const val BASE_URL = "https://api.football-data.org"
+
+const val GET_COMPETION_FEATURES = "/v4/competitions"
+
+const val GET_AREA_FEATURES = "/v4/areas"
+
+const val ACCESS_TOKEN = "5df546bea93f409f9257de63397064d1"
